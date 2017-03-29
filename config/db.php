ddc551448a769e55374d417204b8a34a54d8b8ad
ultramord <bsirka200596@gmail.com>
2017-03-29 11:28:13 +0900

    erwer

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ifmk_pr',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
--- views/site/entry-confirm.php
+++
@@ -1,8 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-<p>Вы ввели следующую информацию:</p>
-
-<ul>
-    <li><label>Name</label>: <?= Html::encode($model->imageFile) ?></li>
-</ul>
